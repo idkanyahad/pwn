37addda270fed1144fb8d11159d20d42c37fb843
chilliEgg <abdulahadkhan6944@gmail.com>
2026-07-12 22:34:59 +0530

    Add assembly program to sum integers from stack
    Read the numbers from argv, convert each one with your atoi, add them into a total, turn that total back into text with your itoa, and write it to standard output. With no number arguments, print 0

---
+++ sum_em_all.s
@@ -0,0 +1,122 @@
+.intel_syntax noprefix
+
+.global _start
+
+_start:
+    mov r13, [rsp]
+    dec r13
+
+    lea r12, [rsp+16]
+    xor rbx, rbx
+
+sum_loop:
+    cmp r13, 0
+    je convert_sum
+
+    mov rdi, [r12]
+    call atoi
+
+    add rbx, rax
+
+    add r12, 8
+    dec r13
+    jmp sum_loop
+
+convert_sum:
+    sub rsp, 32
+
+    mov rdi, rbx
+    mov rsi, rsp
+    call itoa
+
+    mov rdx, rax
+    mov rsi, rsp
+    mov rdi, 1
+    mov rax, 1
+    syscall
+
+    add rsp, 32
+
+    xor rdi, rdi
+    mov rax, 60
+    syscall
+
+atoi:
+    mov rax, 0
+    mov r8, 0
+
+    mov cl, [rdi]
+    cmp cl, '-'
+    jne loop_start
+
+    mov r8, 1
+    inc rdi
+
+loop_start:
+    mov cl, [rdi]
+    sub cl, '0'
+    cmp cl, 9
+    ja done
+
+    imul rax, 10
+    movzx rcx, cl
+    add rax, rcx
+
+    inc rdi
+    jmp loop_start
+
+done:
+    cmp r8, 0
+    je finish
+
+    neg rax
+
+finish:
+    ret
+
+itoa:
+    mov r8, rsi
+    xor r10, r10
+    mov rax, rdi
+
+    cmp rax, 0
+    jge check_zero
+
+    mov byte ptr [r8], '-'
+    inc r8
+    neg rax
+    mov r10, 1
+
+check_zero:
+    cmp rax, 0
+    jne convert
+
+    mov byte ptr [r8], '0'
+    mov rax, 1
+    add rax, r10
+    ret
+
+convert:
+    xor rcx, rcx
+
+loop_div:
+    xor rdx, rdx
+    mov r9, 10
+    div r9
+    add dl, '0'
+    push rdx
+    inc rcx
+    cmp rax, 0
+    jne loop_div
+
+write_digits:
+    pop rdx
+    mov [r8], dl
+    inc r8
+    dec rcx
+    cmp rcx, 0
+    jne write_digits
+
+    mov rax, r8
+    sub rax, rsi
+    ret
